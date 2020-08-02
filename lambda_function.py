fff48ff7a5331a79d009c8c83e4381230e9c91c5
Roqvie <vmandrch@gmail.com>
2020-08-02 12:49:24 +0300

    Delete num_of_attemps

--- lambda_function.py
+++ lambda_function.py
@@ -7,7 +7,6 @@ def lambda_handler(event, context):
     TOKEN = event['token']
     USERNAME = event['username']
     CHANNEL = event['channel']
-    MAX_TRY_COUNT = int(event['max'])
     WORD = event['word']
     NUM_OF_WINNERS = int(event['num_of_winners'])
 
@@ -18,9 +17,8 @@ def lambda_handler(event, context):
     server.send(bytes(f'NICK {USERNAME}\r\n', 'utf-8'))
     server.send(bytes(f'JOIN #{CHANNEL}\r\n', 'utf-8'))
 
-    num_of_attemps = 0
     winners = []
-    while num_of_attemps < MAX_TRY_COUNT:
+    while len(winners) < NUM_OF_WINNERS:
         response = server.recv(2048).decode('utf-8')
         response = response.split('\r\n')
         for user_message in response:
@@ -29,7 +27,6 @@ def lambda_handler(event, context):
                 if match:
                     if WORD in match[0][1] and len(winners) < NUM_OF_WINNERS:
                         winners.append(match[0][0])
-        num_of_attemps += 1
 
     if winners:
         return {
